647ed75ddce7bda05ae70b8e62d64aa0ebb925bf
Kamil Labus <klabus03@gmail.com>
2023-07-26 22:10:14 +0200

    comments and saving on device

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes2/components/note_card.dart';
 import 'package:notes2/screens/new_note.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,11 +10,7 @@ class HomeScreen extends StatefulWidget {
 
 class HomeScreenState extends State<HomeScreen> {
   bool isDarkMode = false;
-  List<String> notes = [
-    'Note 1',
-    'Note 2',
-    'Note 3',
-  ];
+  List<String> notes = [];
 
   void _openMenu(BuildContext context) {
     print('Menu icon clicked!');
@@ -27,24 +22,29 @@ class HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void _addNewNote() async {
-    String? newNote = await Navigator.push(
+  // Przenosi użytkownika na nowy ekran NewNoteScreen, który pozwala na dodawanie nowych notatek.
+  // Po powrocie z tego ekranu do bieżącego ekranu, aktualizuje listę notatek (notes) na podstawie
+  // wyniku zwróconego z ekranu NewNoteScreen.
+  void _addNewNote() {
+    Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => NewNoteScreen()),
-    );
-
-    if (newNote != null) {
-      setState(() {
-        notes.add(newNote);
-      });
-    }
+      MaterialPageRoute(builder: (context) => NewNoteScreen(notes: notes)),
+    ).then((updatedNotes) {
+      if (updatedNotes != null) {
+        setState(() {
+          notes = updatedNotes;
+        });
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //Zmiana tła (jasne/ciemne)
       theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
       home: Scaffold(
+        // Nagłówek aplikacji z przyciskami
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 217, 0, 255),
           title: Row(
@@ -73,19 +73,47 @@ class HomeScreenState extends State<HomeScreen> {
             ],
           ),
         ),
+        // Generowana lista notatek
         body: ListView.builder(
-            itemCount: notes.length,
-            itemBuilder: (context, index) {
-              return NoteCard(
-                note: notes[index],
+          itemCount: notes.length,
+          itemBuilder: (context, index) {
+            final note = notes[index];
+
+            // Sprawdź czy tytuł i treść oddziela nowa linia
+            if (note.contains('\n')) {
+              final title = note.substring(0, note.indexOf('\n'));
+              final description = note.substring(note.indexOf('\n') + 1);
+
+              return Card(
+                elevation: 4,
+                child: ListTile(
+                  title: Center(
+                    child: Text(
+                      title,
+                      style:
+                          const TextStyle(fontSize: 24, fontFamily: 'Handjet'),
+                    ),
+                  ),
+                  subtitle: Text(
+                    description,
+                    style: const TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
               );
-            }),
+            }
+          },
+        ),
+        //Przycisk dodawania notatek uruchamiający funkcję _addNewNote(),
+        //która przenosi użytkownika na nowy ekran dodawania notatki
         floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              _addNewNote();
-            },
-            backgroundColor: const Color.fromARGB(255, 217, 0, 255),
-            child: const Icon(Icons.add)),
+          onPressed: () {
+            _addNewNote();
+          },
+          backgroundColor: const Color.fromARGB(255, 217, 0, 255),
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
--- lib/screens/new_note.dart
+++ lib/screens/new_note.dart
@@ -1,37 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NewNoteScreen extends StatefulWidget {
-  const NewNoteScreen({Key? key}) : super(key: key);
+  final List<String> notes;
+
+  const NewNoteScreen({Key? key, required this.notes}) : super(key: key);
 
   @override
   NewNoteScreenState createState() => NewNoteScreenState();
 }
 
 class NewNoteScreenState extends State<NewNoteScreen> {
-  final TextEditingController _textController = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+  String newNoteTitle = '';
+  String newNoteContent = '';
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('New Note'),
+        title: const Text('Nowa notatka'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
+          // Pole tekstowe tytułu
           children: [
             TextFormField(
-              controller: _textController,
+              controller: _titleController,
               maxLines: null,
               decoration: const InputDecoration(
-                hintText: 'Enter your new note...',
+                hintText: 'Title',
+              ),
+            ),
+            // Pole tekstowe treści
+            TextFormField(
+              controller: _contentController,
+              maxLines: null,
+              decoration: const InputDecoration(
+                hintText: 'Content',
               ),
             ),
             const SizedBox(height: 16),
+            // Przycisk zapisz
             ElevatedButton(
               onPressed: () {
-                _saveNoteAndReturnHome(context);
+                _saveNoteAndReturnHome();
               },
               child: const Text('SAVE'),
             ),
@@ -41,9 +64,39 @@ class NewNoteScreenState extends State<NewNoteScreen> {
     );
   }
 
-  void _saveNoteAndReturnHome(BuildContext context) {
-    String newNote = _textController.text;
+  // Funkcja sprawdzająca i zapisująca tekst z obu ból tekstowych
+  void _saveNoteAndReturnHome() {
+    String newNoteTitle = _titleController.text;
+    String newNoteContent = _contentController.text;
 
-    Navigator.pop(context, newNote);
+    // Zabezpiczenie przed niewpisaniem tytułu lub treści
+    if (newNoteTitle.isEmpty || newNoteContent.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Błąd'),
+          content: const Text('Zapomniałeś/aś tytułu lub treści notatki!'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+
+    //Połączenie tytułu i treści w nowy jeden napis oddzielony 2 nowymi liniami
+    String newNote = '$newNoteTitle\n\n$newNoteContent';
+    List<String> updatedNotes = List.from(widget.notes)..add(newNote);
+
+    //Użycie modułu shared_preferences aby zapisać dane w pamięci urządzenia
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.setStringList('notes', updatedNotes);
+
+      
+      Navigator.pop(context, updatedNotes);
+    });
   }
 }
